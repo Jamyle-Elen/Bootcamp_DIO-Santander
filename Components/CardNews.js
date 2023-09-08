7ffdf015cafba73aaed77cd1d7db085b4fb49d39
Jamyle Elen <jamyleelen09@gmail.com>
2023-09-07 21:05:49 -0300

    cardNews

--- Components/CardNews.js
+++ Components/CardNews.js
@@ -6,6 +6,33 @@ class CardNews extends HTMLElement {
         shadow.innerHTML = "<h1>Hello</h1>"
 
     }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card")
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", card__Left);
+
+        const autor = document.createAttribute("span");
+        const linkTitle = document.createElement("a");
+        const newsContent = document.createElement("p");
+
+        cardLeft.appendChild(autor);
+        linkTitle.appendChild(linkTitle);
+        newsContent.appendChild(newsContent);
+        
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", card__Right)
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot
+    }
+
+    style() {}
 }
 
 customElements.define("card-news", CardNews);
